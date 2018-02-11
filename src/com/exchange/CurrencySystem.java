14d6528d4d680582c1c92d16ec48bfece6c3c93c
sahilpwr <sahil.sandypwr@gmail.com>
2018-02-11 14:26:55 -0500

    User class

--- src/com/exchange/CurrencySystem.java
+++ src/com/exchange/CurrencySystem.java
@@ -5,36 +5,28 @@ import java.util.HashMap;
 public class CurrencySystem 
 {
 	private HashMap<String,User> users=new HashMap<>();
-	private String userName;
-	private String password;
-	private String firstName;
-	private  String lastName;
-	private String  emailID;
+	
 	
 	
 	public String cryptoInfo()
 	{
 		return "";
 	}
-	
-	
 
-	
 
-	public boolean createUser(String username,String password,String firstName, String lastname ,String emailID)
+	public boolean createUser(String userName,String password,String firstName, String lastName ,String emailID)
 	{
-		
-		
-		this.userName = userName;
-		this.password = password;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.emailID = emailID;
+	
 	
 		User newUser=new User();
+		newUser.setEmailID(emailID);
+		newUser.setPassword(password);
+		newUser.setFirstName(firstName);
+		newUser.setLastName(lastName);
+		newUser.setUserName(userName);
 		
 		
-		users.put(newUser., value);
+		users.put(newUser.getUserName(),newUser);
 		
 		return true;
 	}
--- src/com/exchange/User.java
+++ src/com/exchange/User.java
@@ -7,9 +7,65 @@ public class User
  private String  emailID;
  private String password;
  private String[] bankPayment;
- private String[] creditPayemtn;
- private static int userID;
- private double roi;
+ private String[] creditPayemt;
+ private  String userName;
+ 
+ 
+ public  String getUserName() 
+ {
+	return userName;
+}
+
+public void setUserName(String userName) 
+{
+	this.userName = userName;
+}
+
+
+
+public String getFirstName() {
+	return firstName;
+}
+
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+
+public String getLastName() {
+	return lastName;
+}
+
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+
+public String getEmailID() {
+	return emailID;
+}
+
+public void setEmailID(String emailID) {
+	this.emailID = emailID;
+}
+
+public String getPassword() {
+	return password;
+}
+
+public void setPassword(String password) {
+	this.password = password;
+}
+
+public double getRoi() {
+	return roi;
+}
+
+public void setRoi(double roi) {
+	this.roi = roi;
+}
+
+
+
+private double roi;
  
  public void setUser()
  {

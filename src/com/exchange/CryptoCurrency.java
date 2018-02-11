c516b6c33450b28244245da093310a93543bf86a
sahilpwr <sahil.sandypwr@gmail.com>
2018-02-11 16:40:03 -0500

    CheckUser

---
+++ src/com/exchange/CryptoCurrency.java
@@ -0,0 +1,5 @@
+package com.exchange;
+
+public class CryptoCurrency {
+
+}
--- src/com/exchange/CurrencySystem.java
+++ src/com/exchange/CurrencySystem.java
@@ -1,39 +1,52 @@
 package com.exchange;
 
+import java.util.Currency;
 import java.util.HashMap;
 
 public class CurrencySystem 
 {
 	private HashMap<String,User> users=new HashMap<>();
+	private CryptoCurrency[] currency=new CryptoCurrency[3];
 	
 	
-	
-	public String cryptoInfo()
+	public CryptoCurrency[] cryptoInfo()
 	{
-		return "";
+		return currency;
 	}
 
 
 	public boolean createUser(String userName,String password,String firstName, String lastName ,String emailID)
 	{
-	
-	
+		
 		User newUser=new User();
 		newUser.setEmailID(emailID);
 		newUser.setPassword(password);
 		newUser.setFirstName(firstName);
 		newUser.setLastName(lastName);
-		newUser.setUserName(userName);
-		
+		newUser.setUserName(userName);	
 		users.put(newUser.getUserName(),newUser);
 		
 		return true;
 	}
-	public boolean checkUser()
+	
+	public boolean checkUser(String userName,String password)
 	{
-		return true;
+		if(users.containsKey(userName))
+		{
+		     User currentUser=users.get(userName);
+			 if( currentUser.getPassword().equals(password))
+			 {
+			   return true;
+			 }
+	    }
+	    return false;
 	}
 
+	public void checkSchedule()
+	{
+		
+	}
+	
 	public static void main(String[] args) 
 	{
 		

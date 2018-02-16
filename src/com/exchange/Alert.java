cd675e6bcfff90e5a3972d8670dfc472a4ef2366
Vishal Hingorani <vishalhingorani18@gmail.com>
2018-02-16 00:22:12 -0500

    created alert class

--- src/com/exchange/Alert.java
+++ src/com/exchange/Alert.java
@@ -2,4 +2,20 @@ package com.exchange;
 
 public class Alert {
 
+	private double amount;
+	private CryptoCurrency [] cryptoCurrencies ;
+	
+	
+	
+	public String sendAlert() {
+		
+		return "";
+		
+	}
+	
+	public void createAlert() {
+		
+	}
+	
+	
 }
